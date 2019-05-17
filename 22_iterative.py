52a5f3ad17ed36ae8014c64b877e14b15c512e26
Jason Lee <lijiansen.sjtu@gmail.com>
2019-05-17 11:41:52 +0800

    22. Generate parentheses iterative solution

--- 22_iterative.py
+++ 22_iterative.py
@@ -17,4 +17,28 @@
 @desc:
 
 '''
+from typing import List
 
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n < 1:
+            return []
+        left = 0
+        right = 0
+        res = []
+        s = [('', left, right)]
+        while s:
+            string, left, right = s.pop()
+            if left == n and right == n:
+                res.append(string)
+            if left < right or left > n or right > n:
+                continue
+            s.append((string + '(', left + 1, right))
+            s.append((string + ')', left, right + 1))
+
+        return res
+
+if __name__ == '__main__':
+    res = Solution()
+    a = res.generateParenthesis(3)
+    print(a)
