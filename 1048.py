183781a3f3cf8491dcbab213c516374099ce5c78
Jason Lee <lijiansen.sjtu@gmail.com>
2019-05-19 15:19:19 +0800

    1048. Longest string chain

--- 1048.py
+++ 1048.py
@@ -17,4 +17,21 @@
 @desc:
 
 '''
+from typing import List
 
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        if len(words) <= 1:
+            return len(words)
+        dp = {}
+        for w in sorted(words, key=len):
+            for i in range(len(w)):
+                dp[w] = max(dp.get(w, 1), 1 + dp.get(w[:i] + w[i+1:], 0))
+
+        return max(dp.values() or [1])
+
+if __name__ == '__main__':
+    res = Solution()
+    # a = res.check_predecessor('b', 'ba')
+    a = res.longestStrChain(["ksqvsyq","ks","kss","czvh","zczpzvdhx","zczpzvh","zczpzvhx","zcpzvh","zczvh","gr","grukmj","ksqvsq","gruj","kssq","ksqsq","grukkmj","grukj","zczpzfvdhx","gru"])
+    print(a)
