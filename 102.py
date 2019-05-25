95e382d7f55d1654efc4820644cac0a694adc73f
Jason Lee <lijiansen.sjtu@gmail.com>
2019-05-25 21:19:54 +0800

    102. Binary tree level order traversal

---
+++ 102.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+'''
+
+@author: Jason Lee
+
+@license: (C) Copyright @ Jason Lee
+
+@contact: jiansenll@163.com
+
+@file: 102.py
+
+@time: 2019/5/25 21:08
+
+@desc:
+
+'''
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        temp = []
+        while stack:
+            val = []
+            while stack:
+                node = stack.pop()
+                val.append(node.val)
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            stack = temp[::-1]
+            temp = []
+            res.append(val)
+        return res
